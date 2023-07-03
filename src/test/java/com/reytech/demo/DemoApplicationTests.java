cd964eef93006881fb8abc4aa692fa8ad6194ba8
HedDelgado <hedirsondelgado@gmail.com>
2023-07-03 11:35:53 +0200

    Ajout class

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -1,7 +1,9 @@
 package com.reytech.demo;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.reytech.demo.Personne;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +12,12 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+
+	@Test
+
+	void groupAssertions(){
+		Personne pers = new Personne("owl","OWL");
+		assertAll("peronne",()-> assertEquals("owl", pers.getFirstName()),
+				() -> assertEquals("OWL",pers.getLastName()));
+	}
 }
